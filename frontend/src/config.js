edd100fa16b75ab762898dcd966200c1c6ee97a2
Jin Hao <limjinhao@gmail.com>
2021-09-10 01:04:22 +0800

    Add server link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
 export const apiUrl =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:5000";
+  process.env.NODE_ENV === "production"
+    ? "https://notekeeper-3219.herokuapp.com"
+    : "http://localhost:5000";
